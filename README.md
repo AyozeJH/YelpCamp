77dbc5920d022d4df3186f3d18318597fe79a939
unknown <ayozejorgehernandez@gmail.com>
2018-08-13 23:16:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Instructions to run the app:
+
+Reading this document will help you to run the app easily.
+This app uses Node.js and MongoDB. First of all, please make sure you have both intalled.
+
+## Installation
+After dowloading or clonning the project, go to the project folder in your command line and run the following command to install the packages.
+
+```
+npm install / yarn install
+
+```
+
+## Running the app
+To run mongoDB in your command line  you should change your current directory to the bin folder inside your MongoDB folder, and then run the following command.
+
+```
+mongod
+```
+
+(note: in order to run mongoDB you MUST have in your C: a folder named "data" with a folder named "db" inside).
+
+After this, go to the project folder and execute the command:
+
+```
+node app.js
+```
+
+Thank you for downloading it!
---
+++ routes/campgrounds.js
@@ -0,0 +1,71 @@
+const   express     = require("express"),
+        router      = express.Router(),
+        Campground  = require("../models/campground");
+
+//INDEX - show all campgrounds
+router.get("/", function(req, res){
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds/index", {campgrounds: allCampgrounds, currentUser: req.user});
+        }
+    });
+});
+
+//CREATE - add new campground to DB
+router.post("/", isLoggedIn, function(req, res){
+    //get data from form and add to campgrounds array
+    let name = req.body.name;
+    let image = req.body.image;
+    let desc = req.body.description;
+    let author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    let newCampground = {name: name, image: image, description: desc, author: author};
+    //Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    }); 
+});
+
+//NEW - show form to create a new campground
+router.get("/new", isLoggedIn, function(req, res){
+    res.render("campgrounds/new");
+});
+
+//SHOW - shows more info about one campground
+router.get("/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }
+        else {
+            //Show template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+//Edit campground route
+router.get("/:id/edit", function(req, res){
+    res.render("campgrounds/edit");
+});
+
+//Middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
